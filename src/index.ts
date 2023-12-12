b0d3d57f9152ca3a3169187bd2e1a300bedb928c
rebeckaelin <rebeckaelin@hotmail.com>
2023-12-12 19:02:45 +0100

    cleaned up the ts-file

--- src/index.ts
+++ src/index.ts
@@ -8,101 +8,88 @@
 // is flipped, is checked, is correct, flip tillbaka
 // checkIfMatched om first === second
 
-// ******************************************MEMORY****************************************//
-//Grymma vi är ändå
+// ******************************************VARIABLER****************************************//
 let cardValue: any;
-
 let card: any;
 
 type CardObject = {
-    value: string,
-    element: Element
-}
+  value: string;
+  element: Element;
+};
 
 let arr: CardObject[] = [];
 
 // *************DOM****************
 
+let allCards = document.querySelectorAll(".memory-card");
 
-let allCards = document.querySelectorAll('.memory-card');
 // *********** FUNKTIONER ***********
 
 function shuffle() {
-    const memoryCards = document.querySelector(".memory-cards");
-    if (memoryCards) {
-        const cardsArray = Array.from(memoryCards.children);
+  const memoryCards = document.querySelector(".memory-cards");
+  if (memoryCards) {
+    const cardsArray = Array.from(memoryCards.children);
 
-        for (let i = cardsArray.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            memoryCards.appendChild(cardsArray[j])
-        }
+    for (let i = cardsArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      memoryCards.appendChild(cardsArray[j]);
     }
+  }
 }
-
-shuffle()
-
-
-
-allCards.forEach(card => {
-    // console.log(card);
-    let clickedCard = () => {
-
-        if (card.classList.contains("flip")) {
-            return; // If the card is already flipped, do nothing
-        }
-
-        cardValue = card.getAttribute('data-card');
-
-
-
-        flipper(card)
-        arr.push({ value: cardValue, element: card });
-
-        // console.log(arr);
-
-        compareValue(arr)
-
-    }
-
-    card.addEventListener("click", clickedCard)
-
-});
 
 function compareValue(array: CardObject[]) {
-    if (array.length < 2) {
-        return;
-    }
-    let lastIndex = array.length - 1;
-    let secondLastIndex = array.length - 2;
+  if (array.length < 2) {
+    return;
+  }
+  let lastIndex = array.length - 1;
+  let secondLastIndex = array.length - 2;
 
-    if (array[lastIndex].value === array[secondLastIndex].value) {
-        console.log("wohoo");
+  if (array[lastIndex].value === array[secondLastIndex].value) {
+    console.log("wohoo");
 
-        arr.forEach(cardObj => {
-            cardObj.element.classList.add("flip");
-            cardObj.element.removeEventListener("click", cardObj.element['clickedCard']);
-        });
+    arr.forEach((cardObj) => {
+      cardObj.element.classList.add("flip");
+      cardObj.element.removeEventListener(
+        "click",
+        cardObj.element["clickedCard"]
+      );
+    });
 
-        arr.length = 0;
-    } else {
-        console.log("Dum i huvet är du!");
-        setTimeout(() => {
-            arr.forEach(cardObj => {
-                unflip(cardObj.element);
-            });
+    arr.length = 0;
+  } else {
+    console.log("Dum i huvet är du!");
+    setTimeout(() => {
+      arr.forEach((cardObj) => {
+        unflip(cardObj.element);
+      });
 
-            arr.length = 0;
-        }, 1500);
-    }
-
+      arr.length = 0;
+    }, 200);
+  }
 }
 
-const flipper = (param) => {
-    param.classList.add("flip")
-}
+const flip = (param) => {
+  param.classList.add("flip");
+};
 
 const unflip = (param) => {
-    param.classList.remove("flip")
-}
+  param.classList.remove("flip");
+};
 
+// ***KÖR SPELET HÄR NERE***//
 
+shuffle();
+
+allCards.forEach((card) => {
+  let clickedCard = () => {
+    if (card.classList.contains("flip")) {
+      return;
+    }
+    cardValue = card.getAttribute("data-card");
+    flip(card);
+    arr.push({value: cardValue, element: card});
+    compareValue(arr);
+  };
+
+  card.addEventListener("click", clickedCard);
+});
